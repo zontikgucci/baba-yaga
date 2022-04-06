f5f9af252a2848aeb8ff679d4292337da2834c17
Vlad Gubar <dkflbr1@bk.ru>
2022-04-06 16:37:04 +0300

    baby

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// img .ibg
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+ibg();
+
+
+// password
+function show_hide_password(target) {
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
+
+
+function speed_video() {
+	var video = document.getElementById('video-speed');
+	video.playbackRate = .8;
+}
+speed_video();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
