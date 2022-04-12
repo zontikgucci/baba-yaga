478251db1387bdef0b311a42998f78d404ac78aa
Vlad Gubar <dkflbr1@bk.ru>
2022-04-12 12:19:13 +0300

    baby

--- js/main.js
+++ js/main.js
@@ -31,6 +31,17 @@ function speed_video() {
 }
 speed_video();
 
+// img .ibg
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+ibg();
 
 
 
